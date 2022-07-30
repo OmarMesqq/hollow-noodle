07a1ed0423786d82c2ab9d223adf36647f76438c
Omar <34662089+OmarMesqq@users.noreply.github.com>
2022-07-30 15:58:16 -0300

    Made code less bloated
    Printed "Length" string to screen and removed useless newline at the end of file

--- password.cpp
+++ password.cpp
@@ -6,22 +6,19 @@
 int main() {
 
     char foo[] = { 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9','!','"','@','?','$','&','%','_','#','*','^','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
-
+    
     std::random_device rd;
     srand(rd.operator()());
-
-    std::cout << "Hello! How long would you like your password to be?" << std::endl;
+    
+    std::cout << "Hello! How long would you like your password to be?\n";
+    std::cout << "Length: ";
     int x;
     std::cin >> x;
-
-    std::cout << "Your password is: " << std::endl;
-
+    std::cout << "Your password is: \n\n";
+    
     for(int y = 0; y < x; ++y) {
 
         std::cout << foo[random()%73];
-
     }
-    
-    std::cout << "\n";
-    
+
 }
