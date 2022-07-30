1211b2d19560926bdba501c061506c9a361a009b
Omar <34662089+OmarMesqq@users.noreply.github.com>
2022-07-30 17:35:58 -0300

    Created a function to generate password
    Also allowed the user to generate another password, change length of it or exit the program. I've done it using a while(true) loop

--- password.cpp
+++ password.cpp
@@ -1,24 +1,48 @@
 #include <cstdlib>
 #include <iostream>
 #include <random>
+std::string genPassword(int length) {
 
+    char foo[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                  'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '"', '@', '?',
+                  '$', '&', '%', '_', '#', '*', '^', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                  'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+    std::random_device rd;
+    srand(rd.operator()());
+    for (int y = 0; y < length; ++y) {
+        std::cout << foo[random() % 73];
+    }
+
+
+}
 
 int main() {
 
-    char foo[] = { 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9','!','"','@','?','$','&','%','_','#','*','^','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
-    
-    std::random_device rd;
-    srand(rd.operator()());
-    
-    std::cout << "Hello! How long would you like your password to be?\n";
-    std::cout << "Length: ";
-    int x;
-    std::cin >> x;
-    std::cout << "Your password is: \n\n";
-    
-    for(int y = 0; y < x; ++y) {
+    while (true) {
 
-        std::cout << foo[random()%73];
+        std::cout << "\nHello! How long would you like your password to be?\n";
+        std::cout << "Length: ";
+        int length;
+        std::cin >> length;
+        std::cout << "\nYour password is: " << genPassword(length) << std::endl;
+        std::cout << "\nGenerate another password?\n";
+        std::cout << "y (yes), c (change length) or any other key to exit: ";
+        char x;
+        std::cin >> x;
+        if (x == 'y') {
+            std::cout << "\nYour new password is: " << genPassword(length) << std::endl;
+            std::cout << "\nGoing back to the main menu...\n";
+        }
+        else if (x == 'c') {
+            std::cout << "\nOk! How long would you like your new password to be?\n";
+            std::cout << "Length: ";
+            std::cin >> length;
+            std::cout << "Your new password is: " << genPassword(length) << std::endl;
+            std::cout << "\nGoing back to the main menu...\n";
+        }
+        else {
+            std::cout << "\nNice to see you! Bye :)";
+            break;
+        }
     }
-
 }
