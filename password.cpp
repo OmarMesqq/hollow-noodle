e8c0e4c87a2b33c2d78f0e4a50815bac2995d9f9
OmarMesqq <omarmesquita@eq.ufrj.br>
2023-06-24 16:06:55 -0300

    Major update

--- password.cpp
+++ password.cpp
@@ -1,48 +1,78 @@
 #include <cstdlib>
 #include <iostream>
 #include <random>
-std::string genPassword(int length) {
 
-    char foo[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-                  'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '"', '@', '?',
-                  '$', '&', '%', '_', '#', '*', '^', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-                  'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+void genPassword(int length) {
+        	
+    std::string characters = "abcdefghijklmnopqrstuvwxyz0123456789!\"@?$&%_#*^ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     std::random_device rd;
-    srand(rd.operator()());
+    std::mt19937 gen(rd()); 
+    std::uniform_int_distribution<int> dis(0, characters.size() - 1);
+
+    std::string password;
     for (int y = 0; y < length; ++y) {
-        std::cout << foo[random() % 73];
+	    password += characters[dis(gen)];
     }
+    std::cout << password;
+}
 
 
+void interactive(int lastLength) {
+
+	if (lastLength == 0) {
+
+		std::cout << "\nHow long would you like your password to be?\n";
+	        std::cout << "Length: ";
+	        int length;
+        	std::cin >> length;
+	        std::cout << "\nYour password is: \n";
+		genPassword(length);
+	
+		std::cout << "\nGenerate another password?\n";
+        	std::cout << "y (yes), c (change length) or any other key to exit: ";
+		char x; 
+		std::cin >> x;
+
+		if (x == 'y') {
+			interactive(length);
+		}
+		else if (x == 'c') {
+			std::cout << "New length: \n";
+			int length;
+			std::cin >> length;
+			interactive(length);
+		}
+		else {
+			return;
+		}
+
+	}
+	else {
+		std::cout << "Your new password is: ";
+		genPassword(lastLength);
+
+		std::cout << "\nGenerate new password?\n";
+        	std::cout << "y (yes), c (change length) or any other key to exit: ";
+		char x; 
+		std::cin >> x;
+
+		if (x == 'y') {
+			interactive(lastLength);
+		}
+		else if (x == 'c') {
+			std::cout << "New length: \n";
+			int length;
+			std::cin >> length;
+			interactive(length);
+		}
+		else {
+			return;
+		}
+	}
 }
 
 int main() {
-
-    while (true) {
-
-        std::cout << "\nHello! How long would you like your password to be?\n";
-        std::cout << "Length: ";
-        int length;
-        std::cin >> length;
-        std::cout << "\nYour password is: " << genPassword(length) << std::endl;
-        std::cout << "\nGenerate another password?\n";
-        std::cout << "y (yes), c (change length) or any other key to exit: ";
-        char x;
-        std::cin >> x;
-        if (x == 'y') {
-            std::cout << "\nYour new password is: " << genPassword(length) << std::endl;
-            std::cout << "\nGoing back to the main menu...\n";
-        }
-        else if (x == 'c') {
-            std::cout << "\nOk! How long would you like your new password to be?\n";
-            std::cout << "Length: ";
-            std::cin >> length;
-            std::cout << "Your new password is: " << genPassword(length) << std::endl;
-            std::cout << "\nGoing back to the main menu...\n";
-        }
-        else {
-            std::cout << "\nNice to see you! Bye :)";
-            break;
-        }
-    }
+	interactive(0);
+	return 0;
 }
+
